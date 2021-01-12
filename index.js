0ab9830adf5aa82540b2d0473def3d769fc78a9d
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-01-11 21:47:18 -0500

    commit tree b900a3f07e373ed70be0948119fe0ee384ea62c6

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+let drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver(name) {
+    drivers.unshift(name)
+}
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+}
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name)
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveFirstDriver(name) { 
+ drivers.shift(name)
+}
+
+function appendDriver(name){
+let x;
+
+   x = drivers.concat(name)
+   return x
+
+}
+
+function prependDriver(name){
+   return [name, ...drivers]
+}
+
+function appendDriver(name) {
+  return [...drivers, name]
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1)
+}
+
+
+function removeLastDriver() {
+return drivers.slice(0,drivers.length - 1)
+}
